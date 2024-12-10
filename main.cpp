d2075792cd8bb5d94a90c67558c47a7e0adefea3
Artem Varlakov <kkg27466@gmail.com>
2024-12-10 17:29:45 +0500

    Thrird commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num1, num2;
+
+    // Запрашиваем ввод первого числа
+    std::cout << "Введите первое целое число: ";
+    std::cin >> num1;
+
+    // Запрашиваем ввод второго числа
+    std::cout << "Введите второе целое число: ";
+    std::cin >> num2;
+
+    // Выводим введенные числа
+    std::cout << "Вы ввели: " << num1 << " и " << num2 << std::endl;
+
+    return 0;
     return 0;
 }
